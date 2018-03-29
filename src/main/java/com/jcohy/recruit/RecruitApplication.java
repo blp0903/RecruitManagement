facaa813b453e40135326f2bd9fdd9fa434d9b6e
jiachao <jia_chao23@126.com>
2018-03-29 21:59:19 +0800

    first commit

--- src/main/java/com/jcohy/recruit/RecruitApplication.java
+++ src/main/java/com/jcohy/recruit/RecruitApplication.java
@@ -2,11 +2,22 @@ package com.jcohy.recruit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class RecruitApplication {
+public class RecruitApplication extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RecruitApplication.class, args);
+	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+
+		//前台首页
+		registry.addViewController("/").setViewName("login");
+
 	}
 }
